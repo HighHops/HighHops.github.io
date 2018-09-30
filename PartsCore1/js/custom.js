56f183e822d226efaffa09f0320516b2d6bb9d49
Ilya <41462109+hoppiNNN@users.noreply.github.com>
2018-09-30 15:10:04 +0900

    commit
    commit

---
+++ PartsCore1/js/custom.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('.accordion__head').on('click', function () {
+		var el = $(this);
+		el.next('.accordion__body').slideToggle();
+		el.toggleClass('open');
+		return false;
+	});
+	
+});
+
+   
+
+
