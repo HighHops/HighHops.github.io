2df33288cf68e2bf5cccda47f0adf8fcb01d9507
Ilya <41462109+hoppiNNN@users.noreply.github.com>
2018-10-02 01:57:56 +0900

    asd
    asd

---
+++ Main/js/custom.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('.slider-cards').slick({
+		prevArrow: $('.prev2'),
+		nextArrow: $('.next2'),
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4
+	});
+
+
+});
+
+   
+
+
