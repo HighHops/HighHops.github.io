9003f7f82a43428684fd333569ec9d38437ce79b
Ilya <41462109+hoppiNNN@users.noreply.github.com>
2018-10-19 01:32:53 +0900

    123
    123

---
+++ kubet/basket/js/custom.js
@@ -0,0 +1,61 @@
+$(function () {
+
+
+	$(document).ready(
+		function () {
+			$('.popup-btn').on('click', function (event) {
+				event.preventDefault();
+				$('.popup').fadeIn();
+			});
+			$('.popup-close').on('click', function (event) {
+				event.preventDefault();
+				$('.popup').fadeOut();
+			});
+		}
+	);
+
+	$('.styler').styler();
+
+	$('.toogle-menu').on('click', function (event) {
+		event.preventDefault();
+		$('.dropdown-content').toggleClass('open');
+	});
+
+	$('#radio4').on('click', function (event) {
+		event.preventDefault();
+		$('#address').toggleClass('active');
+	});
+
+
+	$('#radio6').on('click', function (event) {
+		event.preventDefault();
+		$('#pay__btn').addClass('active');
+		$('#next__btn').addClass('unactive');
+	});
+
+	$('#radio5').on('click', function (event) {
+		event.preventDefault();
+		$('#pay__btn').removeClass('active');
+		$('#next__btn').removeClass('unactive');
+	});
+	$('#radio7').on('click', function (event) {
+		event.preventDefault();
+		$('#pay__btn').removeClass('active');
+		$('#next__btn').removeClass('unactive');
+	});
+
+
+
+});
+
+
+$('.tabs a').click(function () {
+	$(this).parents('.tab-wrap').find('.tab-count').addClass('hide');
+	$(this).parent().siblings().removeClass('active');
+	var id = $(this).attr('href');
+	$(id).removeClass('hide');
+	$(this).parent().addClass('active');
+	return false
+});
+
+   
