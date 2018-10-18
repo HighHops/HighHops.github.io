9f93eb8f9c3fef1bf6e3f7647ac10b05d5e8cc89
Ilya <41462109+hoppiNNN@users.noreply.github.com>
2018-10-19 01:34:29 +0900

    123
    123

---
+++ kubet/Main/js/custom.js
@@ -0,0 +1,92 @@
+$(function(){
+
+	$('.stock-slider').slick({
+		prevArrow: $('.prev'),
+		nextArrow: $('.stock-slider__btn'),
+		infinite: true,
+		dots: true,
+		dotsClass: "my-dots",
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+
+	$('.reviews-slider').slick({
+		prevArrow: $('.prev3'),
+		nextArrow: $('.next3'),
+		infinite: true,
+		dots: true,
+		dotsClass: "my-dots2",
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+	$('.slider-cards').slick({
+		prevArrow: $('.prev2'),
+		nextArrow: $('.next2'),
+		infinite: true,
+		dots: true,
+		dotsClass: "my-dots2",
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1210,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 770,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					slidesToScroll: 1,
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+	$('.popup-btn').on('click', function (event) {
+		event.preventDefault();
+		$('.popup').fadeIn();
+	});
+	$('.popup-close').on('click', function (event) {
+		event.preventDefault();
+		$('.popup').fadeOut();
+	});
+
+
+
+	$('.seo-info').niceScroll({
+		cursorcolor: '#242424',
+		cursoropacitymin: '1',
+		background: "#eae8e9"
+	});
+
+	
+	$('.toogle-menu').on('click', function (event) {
+		event.preventDefault();
+		$('.dropdown-content').toggleClass('open');
+	});
+
+
+
+
+	$('.accordion__head').on('click', function () {
+		var el = $(this);
+		el.next('.accordion__body').slideToggle();
+		el.toggleClass('open');
+		return false;
+	});
+	
+
+});
+
+
+
+   
+
