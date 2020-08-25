57809cff52bf60cdb0e98a4aedea1a12991cb738
HighHops <hoppinnn@gmail.com>
2020-08-26 08:42:51 +0900

    ads
    ads

--- script.js
+++
@@ -1,5 +0,0 @@
-const mobileBtn = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-mobileBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
