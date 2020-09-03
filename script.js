0947877781ac51f4072bd6edaa39b1b8cb285057
HighHops <hoppinnn@gmail.com>
2020-09-03 14:24:44 +0900

    New design for main site
    Rework site

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const mobileBtn = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-mobileBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
+///// Это нужно было сделать именно в этом файле
+const mobileBtn = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+mobileBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
